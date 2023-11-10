f7d6f221867ae727677c66867bf3a1a9bd41f1b8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-11-10 18:46:26 +0000

    Добавил остальных сотрудников в switch.

--- server/src/pages/user-switch.lisp
+++ server/src/pages/user-switch.lisp
@@ -6,6 +6,7 @@
   (:import-from #:reblocks/html
                 #:with-html)
   (:import-from #:app/models/user
+                #:get-all-users
                 #:user-avatar-url
                 #:get-user
                 #:user-name
@@ -50,7 +51,10 @@
   (title "Переключалка учёток")
 
   ;; TODO: для прода это надо выставить в NIL
-  (let ((allow-for-anonymous t))
+  (let ((allow-for-anonymous t)
+        (users-to-show '((32 "Сотрудник HR")
+                         (22 "Ментор")
+                         (23 "Новый сотрудник"))))
     (with-html
       (cond
         ((or (get-current-user)
@@ -61,13 +65,22 @@
                 (:p "Тут можно переключаться на пользователей с разными ролями, чтобы попробовать разный функционал сайта.")
                 (:p "После выбора пользователя вас \"залогинит\" под его учёткой и перекинет на дашборд."))
 
+               (:h1 :class "text-xl font-bold text-center"
+                    "Сотрудники для просмотра жюри")
                (:div :class "flex flex-col gap-4"
-                     (render (make-user-switch-widget
-                              (get-user 32) "Сотрудник HR"))
-                     (render (make-user-switch-widget
-                              (get-user 22) "Ментор"))
-                     (render (make-user-switch-widget
-                              (get-user 23) "Новый сотрудник")))))
+                     (loop for (user-id title) in users-to-show
+                           for user = (get-user user-id)
+                           do (render (make-user-switch-widget user title))))
+               (:h1 :class "text-xl font-bold text-center"
+                    "Остальные все сотрудники (их завтра уберём)")
+               
+               (:div :class "flex flex-col gap-4"
+                     (loop for user in (get-all-users)
+                           for user-id = (mito:object-id user)
+                           for name = (user-name user)
+                           unless (member user-id users-to-show
+                                          :key #'first)
+                           do (render (make-user-switch-widget user name))))))
         (t
          (:p "Сорян, но тестовая переключалка учёток доступна только залогиновым пользователям."))))))
 
